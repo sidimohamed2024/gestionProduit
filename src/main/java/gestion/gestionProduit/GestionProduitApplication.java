9214dadf0b4ce03e905c4af576f3d52d27e51715
sidimohamed2024 <sidi.mohamed.noman@iscae.mr>
2024-05-04 22:06:06 +0000

    first commit de product serveur

---
+++ src/main/java/gestion/gestionProduit/GestionProduitApplication.java
@@ -0,0 +1,13 @@
+package gestion.gestionProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitApplication.class, args);
+	}
+
+}
